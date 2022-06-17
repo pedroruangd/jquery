146e0b29f9a64d7ef00966f8e551246e23795475
Pedro Ruan <pedroruangd@gmail.com>
2022-06-17 15:14:48 -0300

    aula 18 - correcao do exercicio

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,13 @@
 // instancia jquery e evita conflitos
 // jQuery( function($){
-$(document).ready(function(){
+$(document).ready(function() {
 
     $('.owl-carousel').owlCarousel();
 
     let titulos = $('h4') // tag
-   
+
     let itens = $('.featured-item') // class
-    
+
     let destaques = $('#featured') // id
 
     console.log(titulos.first());
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
     $('.featured-item a').addClass('btn btn-dark stretch-link');
 
-    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>');
     // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
     // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
     // $('.featured-item:first h4').addClass('active')
@@ -27,8 +27,8 @@ $(document).ready(function(){
     // $('.featured-item:first h4').fadeIn(2000)
     // $('.featured-item:first h4').fadeOut()
     //  $('.featured-item:first h4').css('color', '#f00')
-     
-     $('.featured-item h4').dblclick( function(){
+
+    $('.featured-item h4').dblclick(function() {
 
         $(this).css({
             'color': '#f00',
@@ -36,27 +36,34 @@ $(document).ready(function(){
             'font-weight': '100',
         });
 
-     });
+    });
 
-     /*
-      * Manipulação de eventos
-      */
-     $('.featured-item a').on('blur', function(event){
+    /*
+     * Manipulação de eventos
+     */
+    $('.featured-item a').on('blur', function(event) {
 
         event.preventDefault();
 
         alert('Produto esgotado');
 
-     })
-     
-     
-     
-     
+    })
+});
 
+// CAROUSEL 
 
-
-
-
-
-
-})
+$(".owl-carousel").owlCarousel({
+    autoPlay: 3000,
+    items: 1, // THIS IS IMPORTANT
+    responsive: {
+        480: {
+            items: 1
+        }, // from zero to 480 screen width 4 items
+        768: {
+            items: 2
+        }, // from 480 screen widthto 768 6 items
+        1024: {
+            items: 3 // from 768 screen width to 1024 8 items
+        }
+    },
+});
